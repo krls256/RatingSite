bc73c88d45d122c6750103a03287e6bc9c5910b6
Antiselekotin <deksazon8@gmail.com>
2021-01-29 14:10:00 +0200

    current route bug fix

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -33,24 +33,23 @@
                 <img src="{{ asset("img/logo.svg") }}" alt="Главный логотип сайта" class="header__logo mr-1">
                 <p class="header__name">Ремонт Москва</p>
             </a>
-
             <nav class="header__nav header__nav--disabled header__nav--initial">
                 <ul class="header__list">
                     <li class="header__list-item">
                         <a href="{{ route('rating.user.main') }}"
-                           @if(\Request::route() ? \Request::route()->getName() : '' === 'rating.user.main') class="header__list-link header__list-link--selected" @else class="header__list-link" @endif>Рейтинг компаний</a>
+                           @if((\Request::route() ? \Request::route()->getName() : '') === 'rating.user.main') class="header__list-link header__list-link--selected" @else class="header__list-link" @endif>Рейтинг компаний</a>
                     </li>
                     <li class="header__list-item">
                         <a href="{{ route('rating.user.about') }}"
-                           @if(\Request::route() ? \Request::route()->getName() : '' === 'rating.user.about') class="header__list-link header__list-link--selected" @else class="header__list-link" @endif>О проекте</a>
+                           @if((\Request::route() ? \Request::route()->getName() : '') === 'rating.user.about') class="header__list-link header__list-link--selected" @else class="header__list-link" @endif>О проекте</a>
                     </li>
                     <li class="header__list-item">
                         <a href="{{ route('rating.user.articles.index') }}"
-                           @if(\Request::route() ? \Request::route()->getName() : '' === 'rating.user.articles.index') class="header__list-link header__list-link--selected" @else class="header__list-link" @endif>Статьи</a>
+                           @if((\Request::route() ? \Request::route()->getName() : '') === 'rating.user.articles.index') class="header__list-link header__list-link--selected" @else class="header__list-link" @endif>Статьи</a>
                     </li>
                     <li class="header__list-item">
                         <a href="{{ route('rating.user.videos') }}"
-                           @if(\Request::route() ? \Request::route()->getName() : '' === 'rating.user.videos') class="header__list-link header__list-link--selected" @else class="header__list-link" @endif>Видео</a>
+                           @if((\Request::route() ? \Request::route()->getName() : '') === 'rating.user.videos') class="header__list-link header__list-link--selected" @else class="header__list-link" @endif>Видео</a>
                     </li>
                 </ul>
             </nav>
